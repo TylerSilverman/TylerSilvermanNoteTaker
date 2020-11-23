92241e8adf1fe4509380f21f527e4ec4c5c9d4e9
Tyler Silverman <silverman.tyler@gmail.com>
2020-11-23 17:07:56 -0500

    added comments in server section and a function, also changed some grammer in notes.html

--- develop/server.js
+++ develop/server.js
@@ -1,1 +1,26 @@
+//start with dependencies 
 
+
+
+
+//have a variable or const value for  Express
+
+
+
+
+//have something for the PORT to listen for incoming request 
+var PORT = 8080;
+
+const PORT = 
+
+
+
+//sets up for the Express app to handle data parsing
+
+
+
+
+//starts the server to begin to listen to the PORT  and then will console log 
+app.listen(PORT, function () {
+    console.log("serving is trying to listen on: http://localhost" + PORT);
+})
