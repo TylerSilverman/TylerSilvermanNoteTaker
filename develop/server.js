647f49fc59a8e0453a6239f87d4ca910ee6cd217
Tyler Silverman <silverman.tyler@gmail.com>
2020-11-26 13:05:54 -0500

    added the style sheet to the server.js file and also add routes for the api notes

--- develop/server.js
+++ develop/server.js
@@ -3,30 +3,38 @@
 
 //start with dependencies express is the app
 const express = require('express');
-
-//have const value for  Express -- Express is the app and is the main function that is used in this server.js tab. 
-
 const app = express();
-var path = require("path");
+var path = require("path"); // 
+const PORT = process.env.PORT || 3000; //something for the PORT to listen incoming request 
 
-//have something for the PORT to listen for incoming request 
-const PORT = process.env.PORT || 3000;
+
+
+
+
+
+
+
 //sets up for the Express app to handle data parsing
+
 app.get("/", function(req, res){
 
-    // HTML route
    res.sendFile(path.join(__dirname, "public/index.html"))
-})
+}); //ROUTES interact with public index html database:
 
 app.get("/notes", function(req,res){
 
-    // HTML route
     res.sendFile(path.join(__dirname, "public/notes.html"))
-})
+}); // ROUTES interact with public notes html database:
 
-// API ROUTES interact with database: 
-// app.get("/api/notes")
-// app.use(express.static("public"))
+app.get("/api/notes", function(req, res){
+    return res.json();
+}); // API ROUTES interact with notes api database:
+
+app.use(express.static("public")); //using express.static and app.use to pull all the files from the public folder to use for the css styling. 
+
+
+
+
 // require("apiLinks")(app);
 // require("htmlJsLinks")(app);
 
@@ -39,4 +47,4 @@ app.get("/notes", function(req,res){
 //starts the server to begin to listen to the PORT  and then will console log 
 app.listen(PORT, function () {
     console.log("server is listening on: http://localhost:" + PORT);
-})
+}); //starts the server to begin to listen to the PORT  and then will console log 
