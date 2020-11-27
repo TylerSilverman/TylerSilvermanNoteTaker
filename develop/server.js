62a44c2cbf04c5d740af356d4b53542ca445c281
Tyler Silverman <silverman.tyler@gmail.com>
2020-11-27 10:39:50 -0500

    edited de.json file and added id, also edited notes html placeholders, also added function to server.js and cleaned up extra spaces

--- develop/server.js
+++ develop/server.js
@@ -7,10 +7,11 @@ const app = express(); // app is express
 var path = require("path");  
 const PORT = process.env.PORT || 3000; //something for the PORT to listen incoming request 
 const fs = require("fs");
-app.use(express.json()); 
 var dataBase = require("./db/db.json");// getting the db file to make the changes in the notes
-console.log(dataBase); //console log the db.json file
+console.log(dataBase); //console log whats in the db.json file which is located in the db folder then db.json
 
+//the app.use is setting up the function for express to handle the data parsing. 
+app.use(express.json()); //function to call the inforamtion to the body req. 
 
 app.use(express.static("public")); //using express.static and app.use to pull all the files from the public folder to use mostly from the css styling. 
 
@@ -20,24 +21,23 @@ app.get("assets/js/index.js"), function (req, res){
     res.sendFile(path.join(__dirname, "public/assets/js/index.js"))
 }; // express reaching out to the index.js to send a file after the response 
 
-//sets up for the Express app to handle data parsing
-
+//app.get for the index.html from the public folder
 app.get("/", function(req, res){
-
    res.sendFile(path.join(__dirname, "public/index.html"))
 }); //ROUTES interact with public index html database:
 
+//app.get for the notes.html from the public folder
 app.get("/notes", function(req,res){
-
     res.sendFile(path.join(__dirname, "public/notes.html"))
 }); // ROUTES interact with public notes html database:
 
+//app.get for the notes.html response from the public folder
 app.get("/api/notes", function(req, res){
-    return res.json(); // this parses the information the the left side and appends the notes. 
+    return res.json(dataBase); // this parses the information the the left side and appends the notes. // if you add dataBase in the () then it will project on the screen under the notes saved. //
 }); // API ROUTES interact with notes api database:
 
 
-//starts the server to begin to listen to the PORT  and then will console log 
+//starts the server to begin to listen to the PORT from the local host  and then will console log 
 app.listen(PORT, function () {
     console.log("server is listening on: http://localhost:" + PORT);
 }); //starts the server to begin to listen to the PORT  and then will console log 
@@ -53,8 +53,9 @@ app.listen(PORT, function () {
 
 
 //creating a function for the post method to appear on the screen after a note is written and saved. an app post method lets you saved notes that you create and also post the notes on the side. 
-
 app.post("/api/notes", function(req, res){
+
+    //function to write an additional Note
 
 });
 
