39ecb4bd812c40870c32543e294e3af452bafcc4
Tyler Silverman <silverman.tyler@gmail.com>
2020-11-30 13:51:01 -0500

    edited files to try and find the errors

--- routes/htmlRouteJS.js
+++ routes/htmlRouteJS.js
@@ -1,14 +1,16 @@
 //pathway to get the HTML file 
 const path = require("path");  
 
-//app.get for the notes.html from the public folder
-//// ROUTES interact with public notes html database:
-app.get("/notes", function(req,res){
-    res.sendFile(path.join(__dirname, "../develop/public/notes.html"))
-}); 
+module.exports = app => {
+    //app.get for the notes.html from the public folder
+    //// ROUTES interact with public notes html database:
+    app.get("/notes", function(req,res){
+        res.sendFile(path.join(__dirname, "../develop/public/notes.html"))
+    }); 
 
-//app.get for the index.html from the public folder
-//ROUTES interact with public index html database: to see if a route from the path is entered correctly. 
-app.get("*", function(req, res){
-    res.sendFile(path.join(__dirname, "../develop/public/index.html"))
- }); 
+    //app.get for the index.html from the public folder
+    //ROUTES interact with public index html database: to see if a route from the path is entered correctly. 
+    app.get("*", function(req, res){
+        res.sendFile(path.join(__dirname, "../develop/public/index.html"))
+    }); 
+};
