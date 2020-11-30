b7f55d52e3d5175030194ed49da4ef66edc2681c
Tyler Silverman <silverman.tyler@gmail.com>
2020-11-30 16:20:43 -0500

    trying to figure out the errors

--- develop/server.js
+++ develop/server.js
@@ -11,18 +11,13 @@ const app = express(); // app is express
 const path = require("path");  
 const PORT = process.env.PORT || 3000; //something for the PORT to listen incoming request 
 const fs = require("fs");
-const dataBase = require("../develop/db/db.json");// getting the db file to make the changes in the notes
-console.log(dataBase); //console log whats in the db.json file which is located in the db folder then db.json
+let db = require("../develop/db/db.json");// getting the db file to make the changes in the notes
+console.log(db); //console log whats in the db.json file which is located in the db folder then db.json
 const router = require('express').Router();
 console.log(router);
 let addMore = [];
 // //
 const {v4 : uuidv4} = require('uuid')
-
-//starts the server to begin to listen to the PORT from the local host  and then will console log 
-app.listen(PORT, function () {
-    console.log("server is listening on: http://localhost:" + PORT);
-}); //starts the server to begin to listen to the PORT  and then will console log 
 
 //ALL APP.USE 
 
@@ -33,20 +28,14 @@ app.use(express.static("public")); //using express.static and app.use to pull al
 
 app.use(express.urlencoded({extended: true})); //using the app to express acess to the body requirements (req) function 
 
-
 //ALL APP.GET 
 
 app.get("assets/js/index.js"), function (req, res){
     res.sendFile(path.join(__dirname, "public/assets/js/index.js"))
 }; // express reaching out to the index.js to send a file after the response 
 
-//app.get for the index.html from the public folder
-app.get("/", function(req, res){
-   res.sendFile(path.join(__dirname, "public/index.html"))
-}); //ROUTES interact with public index html database:
-
 //app.get for the notes.html from the public folder
-app.get("/notes", function(req,res){
+app.get("/notes", function (req,res){
     res.sendFile(path.join(__dirname, "public/notes.html"))
 }); // ROUTES interact with public notes html database:
 
@@ -58,34 +47,32 @@ app.get("/", function(req, res) {
 //   / API ROUTES interact with notes api database:
 // this parses the information the the left side and appends the notes. // if you add dataBase in the () then it will project on the screen under the notes saved. //
 app.get("/api/notes" , (req, res) => {
-    fs.readFile("../db/db/json", (err, data) =>{
-        if (err){
-            console.log(err)
-        }
-        else {
-            addMore = JSON.parse(data)
-            res.json(addMore)
-        };
-    });
-    return res.json(dataBase);
+    return res.json(db);
 });
 
-//creating a response to the index.html 
-// app.get("*", function (req, res){
-//     res.sendFile(path.join(__dirname, "./public/index.html"));
-// });
-// if ( fs.existsSync( file ) ) {
-//     file = fs.readFileSync( file );
-// }
+let dbNotes  = JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json"), (error, data) =>{
+    if (error){
+        throw error;
+    }
+}));
+
+let addMoreNotes = function (note) {
+    fs.writeFileSync(path.join(__dirname, "/db/db.json"), (error, data) =>{
+        if (error){
+            throw error;
+        };
+    });
+};
 
 //APP.POST FUNCTION
 
 //creating a function for the post method to appear on the screen after a note is written and saved. an app post method lets you saved notes that you create and also post the notes on the side. 
 app.post("/api/notes", function(req, res){
-    if(addMore.length>1){
-        addMore.push((addMore));
-        console.log(req)
-    };
+    let note = req.body;
+    let id = db.length;
+    note.id = id;
+    db.push(note);
+    addMoreNotes(db);
     
 });
 
@@ -93,7 +80,7 @@ app.post("/api/notes", function(req, res){
 
 app.delete('/api/notes/:id', function(req, res){
     const id = req.params.id;
-    fs.readFile(dataBase, "utf8", (error, data)=>{
+    fs.readFile(db, "utf8", (error, data)=>{
         if(error){
             console.log(error);
         }else{
@@ -107,3 +94,30 @@ app.delete('/api/notes/:id', function(req, res){
     })
 
 
+
+//starts the server to begin to listen to the PORT from the local host  and then will console log 
+app.listen(PORT, function () {
+    console.log("server is listening on: http://localhost:" + PORT);
+}); //starts the server to begin to listen to the PORT  and then will console log 
+
+
+
+
+
+
+
+//------------EXTRA NOTES ------// 
+
+// //app.get for the index.html from the public folder
+// app.get("/", function (req, res){
+//    res.sendFile(path.join(__dirname, "public/index.html"))
+// }); //ROUTES interact with public index html database:
+
+
+//creating a response to the index.html 
+// app.get("*", function (req, res){
+//     res.sendFile(path.join(__dirname, "./public/index.html"));
+// });
+// if ( fs.existsSync( file ) ) {
+//     file = fs.readFileSync( file );
+// }
--- routes/apiRouteJS.js
+++ routes/apiRouteJS.js
@@ -1,55 +1,55 @@
-//dependency to have all modules use the app function
-const fs = require("fs");
-const path = require("path"); 
-const app = express(); // app is express 
+// dependency to have all modules use the app function
+// const fs = require("fs");
+// const path = require("path"); 
+// const app = express(); // app is express 
 
-//able to create package for unique ID's
-const {v4 : uuidv4} = require('uuid')
+// //able to create package for unique ID's
+// const {v4 : uuidv4} = require('uuid')
 
-//added array to be able to push notes created to be saved to the JSON files
-let addMore = [];
+// //added array to be able to push notes created to be saved to the JSON files
+// let addMore = [];
 
-const dataBase = require("../develop/db/db.json");// getting the db file to make the changes in the notes
-console.log(dataBase); //console log whats in the db.json file which is located in the db folder then db.json
+// const dataBase = require("../develop/db/db.json");// getting the db file to make the changes in the notes
+// console.log(dataBase); //console log whats in the db.json file which is located in the db folder then db.json
 
-module.exports = app => {
-    //getting the router to get the notes
-    //getting infromation from db.json file
-    app.get("/api/notes", (req, res) => {
-        fs.readFile(dataBase, "utf8", (error, data) => {
-            if (error){
-                console.log(error);
-            } else{
-                //parsing information the the db.json file and then able to create an addMore array.
-                addMore = JSON.parse(data);
-                res.json(addMore);
-            }
-        });
-    });
-    //post the route information 
-    //creating a function for the post method to appear on the screen after a note is written and saved. 
-    app.post("/api/notes", function(req, res){
-        const addMore = req.body; 
-        //creating an array for the unique Id
-        const id = uuidv4();
-        newNotesArray.id = id;
-        //creating the file to read db.json
-        fs.readFile(dataBase, "utf8", (error, data) => {
-            if (error){
-                console.log(error);
-            }else{
-                addMore = JSON.parse(data);
-                addMore.push(newNotesArray);
-                const newNotesArrayObj = JSON.stringify(addMore);
-                fs.writeFile(dataBase, newNotesArrayObj, error => error ? console.log(error) : console.log("Notes Saved"));
-            }
-        })
-        //giving the user the notes requested and then able to go back to the beginning
-        res.json(addMore);
-    });
-    //delete the post route
+// module.exports = app => {
+//     //getting the router to get the notes
+//     //getting infromation from db.json file
+//     app.get("/api/notes", (req, res) => {
+//         fs.readFile(dataBase, "utf8", (error, data) => {
+//             if (error){
+//                 console.log(error);
+//             } else{
+//                 //parsing information the the db.json file and then able to create an addMore array.
+//                 addMore = JSON.parse(data);
+//                 res.json(addMore);
+//             }
+//         });
+//     });
+//     //post the route information 
+//     //creating a function for the post method to appear on the screen after a note is written and saved. 
+//     app.post("/api/notes", function(req, res){
+//         const addMore = req.body; 
+//         //creating an array for the unique Id
+//         const id = uuidv4();
+//         newNotesArray.id = id;
+//         //creating the file to read db.json
+//         fs.readFile(dataBase, "utf8", (error, data) => {
+//             if (error){
+//                 console.log(error);
+//             }else{
+//                 addMore = JSON.parse(data);
+//                 addMore.push(newNotesArray);
+//                 const newNotesArrayObj = JSON.stringify(addMore);
+//                 fs.writeFile(dataBase, newNotesArrayObj, error => error ? console.log(error) : console.log("Notes Saved"));
+//             }
+//         })
+//         //giving the user the notes requested and then able to go back to the beginning
+//         res.json(addMore);
+//     });
+//     //delete the post route
 
 
-}
+// }
 
 
