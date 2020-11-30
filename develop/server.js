39cb403cc92f1385f5d578617052c0973019ca61
Tyler Silverman <silverman.tyler@gmail.com>
2020-11-30 11:40:46 -0500

    added a route folder, with html.js and api.js files to be more organized

--- develop/server.js
+++ develop/server.js
@@ -8,7 +8,6 @@
 //start with dependencies express is the app
 const express = require('express');
 const app = express(); // app is express 
-var path = require("path");  
 const PORT = process.env.PORT || 3000; //something for the PORT to listen incoming request 
 const fs = require("fs");
 var dataBase = require("./db/db.json");// getting the db file to make the changes in the notes
@@ -29,15 +28,6 @@ app.get("assets/js/index.js"), function (req, res){
     res.sendFile(path.join(__dirname, "public/assets/js/index.js"))
 }; // express reaching out to the index.js to send a file after the response 
 
-//app.get for the index.html from the public folder
-app.get("/", function(req, res){
-   res.sendFile(path.join(__dirname, "public/index.html"))
-}); //ROUTES interact with public index html database:
-
-//app.get for the notes.html from the public folder
-app.get("/notes", function(req,res){
-    res.sendFile(path.join(__dirname, "public/notes.html"))
-}); // ROUTES interact with public notes html database:
 
 //app.get for the notes.html response from the public folder
 app.get("/api/notes", function(req, res){
@@ -80,11 +70,5 @@ app.listen(PORT, function () {
     console.log("server is listening on: http://localhost:" + PORT);
 }); //starts the server to begin to listen to the PORT  and then will console log 
 
-// ------------------------------------------------------------------------------------------/
 //need to create a function to parse the information 
-
-
-
 //need to create a function to have another addNote appear after i is created
-
-
